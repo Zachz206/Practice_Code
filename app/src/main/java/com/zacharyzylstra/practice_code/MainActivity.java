6da8e8a6c090b295a450223927d50f12c48930b4
Zachary Zylstra <zylstrazachary@gmail.com>
2018-11-10 11:58:57 -0800

    Created practice template
    Created 5 new subroutines and updated xml to show output of code.

--- app/src/main/java/com/zacharyzylstra/practice_code/MainActivity.java
+++ app/src/main/java/com/zacharyzylstra/practice_code/MainActivity.java
@@ -2,6 +2,7 @@ package com.zacharyzylstra.practice_code;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * Insert practice code here
+         */
+    }
+
+    public void display(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text);
+    }
+
+    public void display(int text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text + "");
+    }
+
+    public void display1(String text) {
+        display(text);
+    }
+
+    public void display2(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view_2);
+        t.setText(text);
+    }
+
+    public void display3(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view_3);
+        t.setText(text);
     }
 }
